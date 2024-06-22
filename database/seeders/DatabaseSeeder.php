54d5175a781c37da6b424ebd4d9a79d286a455f9
NirmaliePerera <154779829+NirmaliePerera@users.noreply.github.com>
2024-06-22 19:59:00 +0530

    User data seeding
    Data seeding for user table is added. Registration is going to be put inside the admin dashboard.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,12 +12,14 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {
+    {   //Registered UsersTableSeeder 
+        $this->call(UsersTableSeeder::class);
+        \App\Models\User::factory(2)->create();
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+    // User::factory()->create([
+    //   'name' => 'Test User',
+    //   'email' => 'test@example.com',
+    // ]);
     }
 }
