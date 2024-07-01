3d044e211d6aa0ef55a139d2b7d3f05c1594a51e
r4reHuman <tharushif01@gmail.com>
2024-07-01 16:52:19 +0530

    product 1st commit
    controller, model, create blade, routes

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $fillable = [  //data that should be stored in the database is given here. and they should matvh with the coloumn names of the respective table.
+        'name',
+        'category',
+        'color',
+        'size',
+        'quantity',
+        'initial_price',
+        'last_rented_price',
+        'description',
+        'image'
+    ];
 }
